b440e8e8879d9fc2902f03a12a67cc16ce859ec6
Sanjar <sanjarfayzullaev99@gmail.com>
2025-02-21 21:01:49 +0500

    start project

---
+++ config/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.conf.urls.static import static
+from config import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include("shop.urls")),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
