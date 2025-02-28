48d9529dc34277c67ecd46c75e1284a01a791eb2
Umid <umidjon@gmail.com>
2025-02-28 20:28:44 +0500

    super user and simple user created

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from shop.models import *
 # Register your models here.
+admin.site.register(CustomUser)
 admin.site.register(Category)
 admin.site.register(SubCategory)
--- shop/models.py
+++ shop/models.py
@@ -1,12 +1,34 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 # Create your models here.
+class CustomUserManager(BaseUserManager):
+   def create_user(self, email,passwod=None, **kwargs):
+      if not email:
+         raise ValueError('Users must have an email address')
+      email = self.normalize_email(email)
+      user = self.model(email=email, **kwargs)
+      user.set_password(passwod)
+      user.save(using=self._db)
+      return user
+   def create_superuser(self,email,password=None,**kwargs):
+      kwargs.setdefault("is_staff",True)
+      kwargs.setdefault("is_superuser",True)
+      if kwargs.get("is_staff") is not True:
+         raise ValueError('Superuser must be True is_staff') 
+      if kwargs.get("is_superuser") is not True:
+         raise ValueError('Superuser must be True is_superuser')
+      return self.create_user(email=email, passwod=password, **kwargs)
+   
+      
+
+
 class CustomUser(AbstractUser):
    COUNTRY_CHOICES = [
       ("UZB", "UZBEKISTAN"),
       ("KAZ", "KAZAKHSTAN"),
       ("KYR", "KIRGIZISTAN")
    ]
+   username = None
    first_name = models.CharField(max_length=50, blank=True, null=True)
    last_name = models.CharField(max_length=50, blank=True, null=True)
    email = models.EmailField(unique=True)
@@ -17,20 +39,27 @@ class CustomUser(AbstractUser):
    city = models.CharField(max_length=50, blank=True, null=True)
    state = models.CharField(max_length=50, blank=True, null=True)
    zipcode = models.CharField(max_length=50, blank=True, null=True)
-   # USERNAME_FIELD = "email"
-   # REQUIRED_FIELDS = []
+   objects = CustomUserManager()
+   USERNAME_FIELD = "email"
+   REQUIRED_FIELDS = []
 class Category(models.Model):
    title = models.CharField(max_length=50)
    image = models.ImageField(upload_to="image/", blank=True, null=True)
    
    def __str__(self):
       return self.title
-      
+   class Meta:
+      verbose_name = "Category" 
+      verbose_name_plural = 'Categories'  
 class SubCategory(models.Model):
    title = models.CharField(max_length=50)
    image = models.ImageField(upload_to="image/", blank=True, null=True)
-   description = models.TimeField(blank=True, null=True)
+   description = models.TextField(blank=True, null=True)
    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
    
    def __str__(self):
       return self.title
+      
+   class Meta:
+      verbose_name = "SubCategory" 
+      verbose_name_plural = 'SubCategories'
